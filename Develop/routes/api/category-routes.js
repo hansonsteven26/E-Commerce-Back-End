23ad1c61410e88c31c7710e3b29632056b1ff5e6
Steven H <smhanson21@gmail.com>
2023-09-19 00:52:27 -0500

    fix delete error

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -35,7 +35,7 @@ router.post('/', async (req, res) => {
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
 
@@ -53,7 +53,7 @@ router.put('/:id', async (req, res) => {
     }
     res.status(200).json(categoryData);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
 
@@ -71,7 +71,7 @@ router.delete('/:id', async (req, res) => {
     }
     res.status(200).json(categoryData);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
 
--- Develop/seeds/index.js
+++ Develop/seeds/index.js
@@ -1,28 +1,23 @@
-const Category = require('../models/Category');
-const Product = require('../models/Product');
-const ProductTag = require('../models/ProductTag');
-const Tag = require('../models/Tag');
-
-const categorySeedData = require('./category-seeds.json');
-const productSeedData = require('./product-seeds.json');
-const productTagSeedData = require('./product-tag-seeds.json');
-const tagSeedData = require('./tag-seeds.json');
+const seedCategories = require('./category-seeds')
+const seedProducts = require('./product-seeds')
+const seedTags = require('./tag-seeds')
+const seedProductTags = require('./product-tag-seeds')
 
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
-  await Category.bulkCreate(categorySeedData);
+  await seedCategories();
   console.log('\n----- CATEGORIES SEEDED -----\n');
 
-  await Product.bulkCreate(productSeedData);
+  await seedProducts();
   console.log('\n----- PRODUCTS SEEDED -----\n');
 
-  await Tag.bulkCreate(tagSeedData);
+  await seedTags();
   console.log('\n----- TAGS SEEDED -----\n');
 
-  await ProductTag.bulkCreate(productTagSeedData);
+  await seedProductTags();
   console.log('\n----- PRODUCT TAGS SEEDED -----\n');
 
   process.exit(0);
